a96ddb34d7c8965d52f3fbe186bb034646a844f3
anqilique <107088774+anqilique@users.noreply.github.com>
2026-03-29 00:18:53 +1300

    Build Controllers

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MERN Notes App
 
+### Tools
+- Visual Studio Code
+- MongoDB Cloud
+- Postman
+
+### Tutorial Vid
+https://youtu.be/F9gB5b4jgOI?si=jkVd_dURG1TAS4H9&t=5173
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,8 @@ const app = express();
 connectDB();
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json());  // middleware
+
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
